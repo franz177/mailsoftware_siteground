c3c1f4e030b7ff189e821dec0b5d83e7e5f669b8
franz177 <giliberti.f@gmail.com>
2021-06-29 10:16:12 +0200

    Aggiunti campi da visualizzare nella tabella del mensile (casa, canale, nominativo, arrivo, partenza, note, citi tax, operatore check-out)

--- mailsoftware/resources/views/frontend/viste/mensile.blade.php
+++ mailsoftware/resources/views/frontend/viste/mensile.blade.php
@@ -6,26 +6,68 @@
 @endsection
 
 @section('content')
-    <div class="row ">
-        <div class="col-sm-12 col-lg-12">
+    <div class="row">
+        <div class="col-lg-12 col-xxl-12">
             <div class="card card-custom card-stretch gutter-b">
-                <div class="card-header flex-wrap border-0 pt-6 pb-0">
-                    <h4>{{ $ciao }}</h4>
+                <div class="card-header border-0 pt-5">
+                    <h3 class="card-title align-items-start flex-column">
+                            <span class="card-label font-weight-bolder text-dark text-uppercase">
+                                Mensile
+                            </span>
+                        <span class="text-muted mt-3 font-weight-bold font-size-sm"></span>
+                    </h3>
                 </div>
-                <div class="card-body">
-                    <div id="chartDonut"></div>
+                <div class="card-body pt-0 pb-3">
+                    <div class="tab-content">
+                        <!--begin::Table-->
+                        <div class="table-responsive" >
+                            <table class="table table-striped table-bordered  dt-responsive" id="sample_20">
+                                <thead>
+                                <tr>
+                                    <th></th>
+                                    <th class="all text-center">
+                                            <span class="svg-icon svg-icon-primary svg-icon-2x">
+                                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                        <rect x="0" y="0" width="24" height="24"/>
+                                                        <path d="M3.95709826,8.41510662 L11.47855,3.81866389 C11.7986624,3.62303967 12.2013376,3.62303967 12.52145,3.81866389 L20.0429,8.41510557 C20.6374094,8.77841684 21,9.42493654 21,10.1216692 L21,19.0000642 C21,20.1046337 20.1045695,21.0000642 19,21.0000642 L4.99998155,21.0000673 C3.89541205,21.0000673 2.99998155,20.1046368 2.99998155,19.0000673 L2.99999828,10.1216672 C2.99999935,9.42493561 3.36258984,8.77841732 3.95709826,8.41510662 Z M10,13 C9.44771525,13 9,13.4477153 9,14 L9,17 C9,17.5522847 9.44771525,18 10,18 L14,18 C14.5522847,18 15,17.5522847 15,17 L15,14 C15,13.4477153 14.5522847,13 14,13 L10,13 Z" fill="#000000"/>
+                                                    </g>
+                                                </svg>
+                                            </span>
+                                    </th>
+                                    <th class="all text-center">
+                                            <span class="svg-icon svg-icon-primary svg-icon-2x">
+                                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                        <rect x="0" y="0" width="24" height="24"/>
+                                                        <path d="M13,18.9450712 L13,20 L14,20 C15.1045695,20 16,20.8954305 16,22 L8,22 C8,20.8954305 8.8954305,20 10,20 L11,20 L11,18.9448245 C9.02872877,18.7261967 7.20827378,17.866394 5.79372555,16.5182701 L4.73856106,17.6741866 C4.36621808,18.0820826 3.73370941,18.110904 3.32581341,17.7385611 C2.9179174,17.3662181 2.88909597,16.7337094 3.26143894,16.3258134 L5.04940685,14.367122 C5.46150313,13.9156769 6.17860937,13.9363085 6.56406875,14.4106998 C7.88623094,16.037907 9.86320756,17 12,17 C15.8659932,17 19,13.8659932 19,10 C19,7.73468744 17.9175842,5.65198725 16.1214335,4.34123851 C15.6753081,4.01567657 15.5775721,3.39010038 15.903134,2.94397499 C16.228696,2.49784959 16.8542722,2.4001136 17.3003976,2.72567554 C19.6071362,4.40902808 21,7.08906798 21,10 C21,14.6325537 17.4999505,18.4476269 13,18.9450712 Z" fill="#000000" fill-rule="nonzero"/>
+                                                        <circle fill="#000000" opacity="0.3" cx="12" cy="10" r="6"/>
+                                                    </g>
+                                                </svg>
+                                            </span>
+                                    </th>
+                                    <th class="all">nominativo</th>
+                                    <th class="all">Arrivo</th>
+                                    <th class="all">Partenza</th>
+                                    <th class="none">Note</th>
+                                    <th class="all">CityTax</th>
+                                    <th class="all">Op. Check Out</th>
+                                </tr>
+                                </thead>
+
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
 @endsection
 
 {{-- Scripts Section --}}
 @section('scripts')
     <script src="{{ asset('js/pages/widgets.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/datatables/datatables.js') }}" type="text/javascript"></script>
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 
     <script type="text/javascript">
 
@@ -38,6 +80,129 @@
                 }
             });
 
+            var houses_color = {!! $houses_color !!};
+            var houses_typo = {!! $houses_typo !!};
+            var sites_kross = {!! $sites_kross !!};
+            var op_check_out = {!! $op_check_out !!};
+
+            var table = $('#sample_20');
+
+            var oTable = table.DataTable({
+                // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+                "language": {
+                    "aria": {
+                        "sortAscending": ": attivare per ordinare la colonna in ordine crescente",
+                        "sortDescending": ": attivare per ordinare la colonna decrescente"
+                    },
+                    "emptyTable": "Nessun dato trovato nella Tabella",
+                    "info": "Visualizzati da _START_ a _END_ di _TOTAL_ record",
+                    "infoEmpty": "Nessuna voce trovata",
+                    "infoFiltered": "(Filtrato 1 di _MAX_ record totali)",
+                    "lengthMenu": "_MENU_ Righe",
+                    "search": "Cerca:",
+                    "zeroRecords": "Nessuna corrispondenza trovata"
+                },
+
+                processing: true,
+                serverSide: true,
+
+                ajax: {
+                    url:"{{ route('viste.data') }}"
+                },
+
+                columns: [
+                    {
+                        className:      '',
+                        orderable:      false,
+                        searchable:     false,
+                        data:           null,
+                        defaultContent: '',
+                        cellType: "th",
+                        sortable: false
+                    },
+                    {
+                        data: 'casa',
+                        render: function (data, type, row)
+                        {
+                            var house = houses_typo[data];
+                            return house;
+                        },
+                        sortable: false,
+                    },
+                    {data: 'sito',
+                        render: function (data, type, row)
+                        {
+                            if(data in sites_kross){
+                                var site = sites_kross[data];
+                            } else {
+                                var site = data;
+                            }
+                            return site;
+                        }, className:'text-center', sortable: false,
+                    },
+                    {data: 'header', className: 'text-capitalize'},
+                    {data: 'tx_mask_p_data_arrivo'},
+                    {data: 'tx_mask_p_data_partenza'},
+                    {data: 'tx_mask_t1_op_note'},
+                    {data: 'city_tax'},
+                    {data: 'tx_mask_t1_op_checkout',
+                        render: function (data, type, row)
+                        {
+                            if(data in op_check_out) {
+                                var operatore = op_check_out[data];
+                            } else {
+                                var operatore = 'NaN';
+                            }
+                            return operatore;
+                        },
+                    },
+
+                ],
+
+                rowCallback: function(row, data, index) {
+                    $('td:eq(0)', row).addClass('bg-'+houses_color[data.casa]);
+                    $('td:eq(2)', row).addClass('alert-warning text-left');
+                    $('td:eq(3)', row).addClass('alert-warning');
+                    $('td:eq(4)', row).addClass('alert-danger');
+                },
+
+                // setup buttons extentension: http://datatables.net/extensions/buttons/
+                buttons: [
+                    // { extend: 'print', className: 'btn dark btn-outline' },
+                    // { extend: 'pdf', className: 'btn green btn-outline' },
+                    { extend: 'excel', className: 'btn purple btn-outline ' }
+                ],
+
+                // setup responsive extension: http://datatables.net/extensions/responsive/
+                // setup responsive extension: http://datatables.net/extensions/responsive/
+                responsive: {
+                    details: {
+                        type: 'column',
+                        target: 'th',
+                    }
+                },
+                columnDefs: [
+                    { className: 'control', targets:   0, width: '5%' }, //plus
+                    { width: '5%', targets: 1}, //house
+                    { width: '5%', targets: 2}, //sito
+                    { width: '20%', targets: 3}, //nominativo
+                    { width: '10%', targets: 4}, //data-arrivo
+                    { width: '10%', targets: 5}, //data-partenza
+                    { width: '15%', targets: 6}, //note
+                    { width: '5%', targets: 7}, //city_tax
+                    { width: '10%', targets: 8}, //op_check_out
+                ],
+
+                "lengthMenu": [
+                    [5, 10, 15, 20, -1],
+                    [5, 10, 15, 20, "All"] // change per page values here
+                ],
+
+                // set the initial value
+                "pageLength": -1,
+
+                "dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'B f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>", // horizobtal scrollable datatable
+            });
         });
 
     </script>

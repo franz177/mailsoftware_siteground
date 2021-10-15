7d725051f52b3c6262fee22ba6c1d76bb0646788
franz177 <giliberti.f@gmail.com>
2021-10-15 18:09:20 +0200

    Removed ShouldQueue

--- mailsoftware/app/Listeners/Booking/LogBookingListener.php
+++ mailsoftware/app/Listeners/Booking/LogBookingListener.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 
-class LogBookingListener implements ShouldQueue
+class LogBookingListener
 {
     /**
      * Create the event listener.

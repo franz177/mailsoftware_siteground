106331afe121b84664ab8bd001224448c8579269
franz177 <giliberti.f@gmail.com>
2021-10-20 18:49:36 +0200

    Added method for get data from bookings table

--- mailsoftware/app/Http/Controllers/Frontend/Views/SimonettaController.php
+++ mailsoftware/app/Http/Controllers/Frontend/Views/SimonettaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend\Views;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
 use App\Models\Typo;
 use App\Models\TypoCHouse;
 use App\Models\TypoRange;
@@ -24,41 +25,57 @@ class SimonettaController extends Controller
         return view('frontend.viste.mensile.simonetta')->with(compact('message'));
     }
 
-    public function getDataTable()
-    {
-        $year = now()->year;
+    private $sum_tot_pulizie;
 
-        $data = Typo::select([
-                    Typo::raw('LCASE(tt_content.header) as headerl'),
-                    Typo::raw('IFNULL(tt_content.tx_mask_p_casa,0) casa'),
-                    Typo::raw('SUM(tt_content.tx_mask_t3_p_stay) tot_stay'),
-                    Typo::raw('tt_content.tx_mask_t5_kross_cod_channel as sito'),
-                    'tt_content.tx_mask_p_data_arrivo', 'tt_content.tx_mask_p_data_partenza', 'tt_content.tx_mask_t1_op_note',
-                    'tx_mask_t3_p_city_tax_amount as city_tax', 'tx_mask_t1_op_pulizie', 'tx_mask_t0_fattura', 'tx_mask_t3_p_extra_p', 'tx_mask_t1_ore_pulizie', 'tx_mask_t1_op_checkout',
-                    'tx_mask_t3_p_cash_op_cout as cash_operatore_co','tx_mask_t3_p_cash_simo as cash_simo_co', 'tx_mask_t3_p_stay', 'tx_mask_t3_p_s_ex_checkout', 'tx_mask_t3_p_s_checkout'
-                ])
-            ->where('tt_content.CType', 'mask_db_alg_pren')
-            ->where('tt_content.hidden', '=', 0)
-            ->where('tt_content.deleted', '=', 0)
+    public function getDataTables(Request $request)
+    {
+        $month = $request->month ? $request->month : now()->month;
+        $year = $request->year ? $request->year : now()->year;
+
+        $data = Booking::select([
+            Booking::raw('MONTH(tx_mask_p_data_arrivo) as month'),
+            Booking::raw('SUM(totale_pulizie) as totale_pulizie'),
+            Booking::raw('SUM(costo_co) as costo_co'),
+            Booking::raw('SUM(costi_costo_operatore_cambio_biancheria) as costi_costo_operatore_cambio_biancheria'),
+            Booking::raw('SUM(tx_mask_t3_p_s_ex_checkout) as tx_mask_t3_p_s_ex_checkout'),
+            Booking::raw('SUM(tx_mask_p_perc_importo_fisso) as tx_mask_p_perc_importo_fisso'),
+            ])
             ->where(function ($q) use ($year) {
-                $q->where(Typo::raw('YEAR(tt_content.tx_mask_p_data_arrivo)'), '=', $year)
-                    ->orWhere(Typo::raw('YEAR(tt_content.tx_mask_p_data_partenza)'), '=', $year);
+            $q->where(Typo::raw('YEAR(tx_mask_p_data_arrivo)'), '=', 2019)
+                ->orWhere(Typo::raw('YEAR(tx_mask_p_data_partenza)'), '=', 2019);
             })
-            ->where('tt_content.tx_mask_cod_reservation_status', '!=', "CANC")
-            ->groupBy(Typo::raw('MONTH(tt_content.tx_mask_p_data_arrivo)'))
-            ->orderBy(Typo::raw('MONTH(tt_content.tx_mask_p_data_arrivo)'), 'ASC')
-            ->orderBy('tt_content.tx_mask_p_data_partenza', 'ASC')
+//            ->where(function ($q) use ($month) {
+//                $q->where(Typo::raw('MONTH(tx_mask_p_data_arrivo)'), '=', $month)
+//                    ->orWhere(Typo::raw('MONTH(tx_mask_p_data_partenza)'), '=', $month);
+//            })
+            ->where('tx_mask_cod_reservation_status', '!=', "CANC")
+            ->orderBy('tx_mask_p_data_arrivo', 'ASC')
+            ->orderBy('tx_mask_p_data_partenza', 'ASC')
+            ->groupBy(Booking::raw('MONTH(tx_mask_p_data_arrivo)'))
             ->get();
 
         return Datatables::of($data)
             ->addColumn('month', function ($row) {
-                return Carbon::createFromFormat('d-m-Y', $row->tx_mask_p_data_arrivo)->format('M');
+                return $row->month;
             })
-            ->addColumn('stay', function ($row) {
-                return $row->tot_stay;
+            ->addColumn('totale_pulizie', function ($row) {
+                return '<span class="font-weight-bolder">€ '.number_format($row->totale_pulizie, 2, ',', '.').'</span>';
+            })
+            ->addColumn('costo_co', function ($row) {
+                return '<span class="font-weight-bolder">€ '.number_format($row->costo_co, 2, ',', '.').'</span>';
+            })
+            ->addColumn('costi_costo_operatore_cambio_biancheria', function ($row) {
+                return '<span class="font-weight-bolder">€ '.number_format($row->costi_costo_operatore_cambio_biancheria, 2, ',', '.').'</span>';
+            })
+            ->addColumn('tx_mask_t3_p_s_ex_checkout', function ($row) {
+                return '<span class="font-weight-bolder">€ '.number_format($row->tx_mask_t3_p_s_ex_checkout, 2, ',', '.').'</span>';
+            })
+            ->addColumn('tx_mask_p_perc_importo_fisso', function ($row) {
+                return '<span class="font-weight-bolder">€ '.number_format($row->tx_mask_p_perc_importo_fisso, 2, ',', '.').'</span>';
             })
 
-            ->rawColumns(['month', 'stay'])
+            ->rawColumns(['month', 'totale_pulizie', 'costo_co', 'costi_costo_operatore_cambio_biancheria', 'tx_mask_t3_p_s_ex_checkout', 'tx_mask_p_perc_importo_fisso'])
             ->make(true);
     }
+
 }

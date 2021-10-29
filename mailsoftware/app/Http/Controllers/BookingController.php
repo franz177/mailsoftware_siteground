72e227a907e9cea47a6c811680d6da02846adae3
franz177 <giliberti.f@gmail.com>
2021-10-29 08:41:20 +0200

    Test code

--- mailsoftware/app/Http/Controllers/BookingController.php
+++ mailsoftware/app/Http/Controllers/BookingController.php
@@ -34,8 +34,20 @@ class BookingController extends Controller
             ->orderBy('uid', 'desc')
             ->get();
 
+        $booking = Booking::find(996);
+//        return $booking;
 
+        $arrivo = Carbon::createFromFormat('d-m-y', $booking->tx_mask_p_data_arrivo)->format('Y-m-d');
 
+        $c_house = TypoCHouse::select('tx_mask_t4_govin_cin_base','tx_mask_t4_govin_cin_specializzato', 'tx_mask_t4_govin_cin_loco')
+            ->join('tx_mask_c_cos_periodo', 'tx_mask_c_cos_periodo.parentid', '=', 'tt_content.uid')
+            ->whereIn('tt_content.tx_mask_c_cod_feuser', [$booking->tx_mask_t1_op_chechin])
+            ->where('tt_content.tx_mask_c_cod_casa', 'like', '%'. $booking->tx_mask_p_casa .'%')
+            ->where('tx_mask_c_cos_periodo.tx_mask_c_cod_from', '<=', $arrivo)
+            ->where('tx_mask_c_cos_periodo.tx_mask_c_cod_to', '>=', $arrivo)
+            ->where('tt_content.deleted', '=', 0)
+            ->first();
+        return $c_house;
 
         return Booking::find(1525);
 

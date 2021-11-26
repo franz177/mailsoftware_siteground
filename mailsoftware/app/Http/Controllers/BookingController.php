764450cd990868c97dc21b8645228ee7c67cf030
franz177 <giliberti.f@gmail.com>
2021-11-26 18:50:58 +0100

    Changed Query and added columns to update

--- mailsoftware/app/Http/Controllers/BookingController.php
+++ mailsoftware/app/Http/Controllers/BookingController.php
@@ -30,41 +30,70 @@ class BookingController extends Controller
         $bookings = Typo::where('CType','mask_db_alg_pren')
             ->where('hidden', 0)
             ->where('deleted', 0)
-            ->where(Typo::raw('MONTH(FROM_UNIXTIME(tstamp))'), '=', $month)
-            ->orderBy('uid', 'desc')
-            ->get();
-
-        $booking = Booking::find(996);
-//        return $booking;
-
-        $arrivo = Carbon::createFromFormat('d-m-y', $booking->tx_mask_p_data_arrivo)->format('Y-m-d');
-
-        $c_house = TypoCHouse::select('tx_mask_t4_govin_cin_base','tx_mask_t4_govin_cin_specializzato', 'tx_mask_t4_govin_cin_loco')
-            ->join('tx_mask_c_cos_periodo', 'tx_mask_c_cos_periodo.parentid', '=', 'tt_content.uid')
-            ->whereIn('tt_content.tx_mask_c_cod_feuser', [$booking->tx_mask_t1_op_chechin])
-            ->where('tt_content.tx_mask_c_cod_casa', 'like', '%'. $booking->tx_mask_p_casa .'%')
-            ->where('tx_mask_c_cos_periodo.tx_mask_c_cod_from', '<=', $arrivo)
-            ->where('tx_mask_c_cos_periodo.tx_mask_c_cod_to', '>=', $arrivo)
-            ->where('tt_content.deleted', '=', 0)
+//            ->where(Typo::raw('MONTH(FROM_UNIXTIME(tstamp))'), '=', $month)
+//            ->orderBy('uid', 'desc')
+//            ->get();
+            ->where('uid', 996)
             ->first();
-        return $c_house;
 
-        return Booking::find(1525);
+        $arrivo = Carbon::createFromFormat('d-m-Y', $bookings->tx_mask_p_data_arrivo)->format('Y-m-d');
+        $partenza = Carbon::createFromFormat('d-m-Y', $bookings->tx_mask_p_data_partenza)->format('Y-m-d');
+
+        $id_op_co = [
+            $bookings['tx_mask_t1_op_chechin'] ? $bookings['tx_mask_t1_op_chechin'] : 19
+        ];
+
+        $id_house = $bookings['tx_mask_p_casa'];
+
+        $user = TypoUser::where('uid',$id_op_co)->first();
+
+        if($user->deleted != 1){
+            $c_house = TypoCHouse::select('tt_content.uid', 'tt_content.header', 'tt_content.tx_mask_t4_govin_cin_base', 'tt_content.tx_mask_t4_govin_cin_specializzato',
+                'tx_mask_t4_govin_cin_loco')
+                ->join('tx_mask_c_cos_periodo', 'tx_mask_c_cos_periodo.parentid', '=', 'tt_content.uid')
+//                ->where('tt_content.tx_mask_c_cod_feuser', 'like', '%'. $id_op_co .'%')
+                ->whereIn('tt_content.tx_mask_c_cod_feuser', $id_op_co)
+                ->where('tt_content.tx_mask_c_cod_casa', 'like', '%'. $id_house .'%')
+                ->where('tx_mask_c_cos_periodo.tx_mask_c_cod_from', '<=', $arrivo)
+                ->where('tx_mask_c_cos_periodo.tx_mask_c_cod_to', '>=', $partenza)
+                ->where('tt_content.hidden', 0)
+                ->where('tt_content.deleted', 0)
+                ->first();
+
+            if($c_house){
+                switch ($bookings['tx_mask_t1_op_tipo_checkin']){
+                    case 1:
+                        $check_in = $c_house['tx_mask_t4_govin_cin_base'];
+                        break;
+                    case 2:
+                        $check_in = $c_house['tx_mask_t4_govin_cin_specializzato'];
+                        break;
+                    case 3:
+                        $check_in = $c_house['tx_mask_t4_govin_cin_loco'];
+                        break;
+                    default:
+                        $check_in = 0;
+                        break;
+                }
+            } else {
+                $check_in = -1;
+            }
+        } else {
+            $check_in = -99;
+        }
+
+        return $check_in;
+
+        $booking = Booking::find(1421);
+
+
+
+        return Booking::find(1421);
 
     }
 
     public function force()
     {
-//        $bookings = Typo::where('CType','mask_db_alg_pren')
-//            ->where('hidden', 0)
-//            ->where('deleted', 0)
-//            ->orderBy('uid', 'desc')
-//            ->get();
-//
-//        $bookings->each(function($booking) {
-//            BookingSincronizationJob::dispatch($booking);
-//        });
-
         BookingSincronizationJob::dispatch();
 
         Artisan::call('queue:work',['--stop-when-empty' => 1]);
--- mailsoftware/app/Jobs/BookingSincronizationJob.php
+++ mailsoftware/app/Jobs/BookingSincronizationJob.php
@@ -37,10 +37,21 @@ class BookingSincronizationJob implements ShouldQueue
     {
 
         $month = now()->month;
+        $month_before = $month - 1;
+        $months = [
+            $month_before,
+            $month
+        ];
+        $year = now()->year;
         $bookings = Typo::where('CType','mask_db_alg_pren')
             ->where('hidden', 0)
             ->where('deleted', 0)
-            ->where(Typo::raw('MONTH(FROM_UNIXTIME(tstamp))'), '=', $month)
+            ->whereIn(Typo::raw('MONTH(FROM_UNIXTIME(tstamp))'), $months)
+            ->where(Typo::raw('YEAR(FROM_UNIXTIME(tstamp))'), '=', $year)
+            ->whereNotNull('tx_mask_p_sito')
+            ->whereNotNull('tx_mask_p_casa')
+            ->where(Typo::raw('YEAR(tx_mask_p_data_arrivo)'), '>', 2020)
+            ->where('header', 'not like', "%test%")
             ->orderBy('uid', 'desc')
             ->get();
 
@@ -68,6 +79,7 @@ class BookingSincronizationJob implements ShouldQueue
                             'tx_mask_t1_op_chechin' => $booking['tx_mask_t1_op_chechin'], 'tx_mask_t1_op_checkout' => $booking['tx_mask_t1_op_checkout'],
                             'tx_mask_t1_op_manutentore' => $booking['tx_mask_t1_op_manutentore'], 'tx_mask_t1_op_note' => $booking['tx_mask_t1_op_note'],
                             'tx_mask_t1_op_pulizie' => $booking['tx_mask_t1_op_pulizie'], 'tx_mask_t1_op_tipo_checkin' => $booking['tx_mask_t1_op_tipo_checkin'],
+                            'tx_mask_t1_op_costo_extra_cambio_biancheria' => $booking['tx_mask_t1_op_costo_extra_cambio_biancheria'],
                             'tx_mask_t1_ora_checkin' => $booking['tx_mask_t1_ora_checkin'], 'tx_mask_t1_ora_checkout' => $booking['tx_mask_t1_ora_checkout'],
                             'tx_mask_t1_ore_pulizie' => $booking['tx_mask_t1_ore_pulizie'],
                             'tx_mask_t2_p_bianc' => $booking['tx_mask_t2_p_bianc'], 'tx_mask_t2_p_c_extra_b' => $booking['tx_mask_t2_p_c_extra_b'],
@@ -93,6 +105,9 @@ class BookingSincronizationJob implements ShouldQueue
                             'totale_pulizie' => 0,
                             'costo_co' => 0,
                             'mancia_cli' => 0,
+                            'prev_di_cui_pulizie_cliente' => 0,
+                            'prev_tot_extra_cash_co' => 0,
+                            'costi_check_in_self_check_in' => 99,
                             'costi_costo_operatore_cambio_biancheria' => 0,
                             'costi_costo_kit' => 0,
                         ]);
@@ -111,112 +126,5 @@ class BookingSincronizationJob implements ShouldQueue
 
             });
         }
-//        $bookings->chunk(200);
-
-//        if($bookings) {
-//            $bookings->each(function ($booking) {
-//                try {
-//                    $store = Booking::firstOrNew(['uid' => $booking['uid']]);
-//
-//                    $store->hidden = $booking['hidden'];
-//                    $store->deleted = $booking['deleted'];
-//                    $store->header = $booking['header'];
-//                    $store->subheader = $booking['subheader'];
-//                    $store->tx_mask_cod_reservation_status = $booking['tx_mask_cod_reservation_status'];
-//                    $store->tx_mask_contatto_riferimento = $booking['tx_mask_contatto_riferimento'];
-//                    $store->tx_mask_doc_inviati = $booking['tx_mask_doc_inviati'];
-//                    $store->tx_mask_p_casa = $booking['tx_mask_p_casa'];
-//                    $store->tx_mask_p_culla = $booking['tx_mask_p_culla'];
-//                    $store->tx_mask_p_data_arrivo = $booking['tx_mask_p_data_arrivo'];
-//                    $store->tx_mask_p_data_partenza = $booking['tx_mask_p_data_partenza'];
-//                    $store->tx_mask_p_data_prenotazione = $booking['tx_mask_p_data_prenotazione'];
-//                    $store->tx_mask_p_note_noi = $booking['tx_mask_p_note_noi'];
-//                    $store->tx_mask_p_old_uid = $booking['tx_mask_p_old_uid'];
-//                    $store->tx_mask_p_override_perc = $booking['tx_mask_p_override_perc'];
-//                    $store->tx_mask_p_perc_importo_fisso = $booking['tx_mask_p_perc_importo_fisso'];
-//                    $store->tx_mask_p_perc_sito = $booking['tx_mask_p_perc_sito'];
-//                    $store->tx_mask_p_sito = $booking['tx_mask_p_sito'];
-//                    $store->tx_mask_p_token = $booking['tx_mask_p_token'];
-//                    $store->tx_mask_p_tot_ospiti = $booking['tx_mask_p_tot_ospiti'];
-//                    $store->tx_mask_p_under_12 = $booking['tx_mask_p_under_12'];
-//                    $store->tx_mask_t0_cognome = $booking['tx_mask_t0_cognome'];
-//                    $store->tx_mask_t0_country = $booking['tx_mask_t0_country'];
-//                    $store->tx_mask_t0_email = $booking['tx_mask_t0_email'];
-//                    $store->tx_mask_t0_fattura = $booking['tx_mask_t0_fattura'];
-//                    $store->tx_mask_t0_lingua = $booking['tx_mask_t0_lingua'];
-//                    $store->tx_mask_t0_newsletter = $booking['tx_mask_t0_newsletter'];
-//                    $store->tx_mask_t0_sardegna = $booking['tx_mask_t0_sardegna'];
-//                    $store->tx_mask_t0_tel = $booking['tx_mask_t0_tel'];
-//                    $store->tx_mask_t1_op_cambio_biancheria = $booking['tx_mask_t1_op_cambio_biancheria'];
-//                    $store->tx_mask_t1_op_chechin = $booking['tx_mask_t1_op_chechin'];
-//                    $store->tx_mask_t1_op_checkout = $booking['tx_mask_t1_op_checkout'];
-//                    $store->tx_mask_t1_op_manutentore = $booking['tx_mask_t1_op_manutentore'];
-//                    $store->tx_mask_t1_op_note = $booking['tx_mask_t1_op_note'];
-//                    $store->tx_mask_t1_op_pulizie = $booking['tx_mask_t1_op_pulizie'];
-//                    $store->tx_mask_t1_op_tipo_checkin = $booking['tx_mask_t1_op_tipo_checkin'];
-//                    $store->tx_mask_t1_ora_checkin = $booking['tx_mask_t1_ora_checkin'];
-//                    $store->tx_mask_t1_ora_checkout = $booking['tx_mask_t1_ora_checkout'];
-//                    $store->tx_mask_t1_ore_pulizie = $booking['tx_mask_t1_ore_pulizie'];
-//                    $store->tx_mask_t2_p_bianc = $booking['tx_mask_t2_p_bianc'];
-//                    $store->tx_mask_t2_p_c_extra_b = $booking['tx_mask_t2_p_c_extra_b'];
-//                    $store->tx_mask_t2_p_c_extra_kit = $booking['tx_mask_t2_p_c_extra_kit'];
-//                    $store->tx_mask_t2_p_cambi_a = $booking['tx_mask_t2_p_cambi_a'];
-//                    $store->tx_mask_t2_p_cambi_aut = $booking['tx_mask_t2_p_cambi_aut'];
-//                    $store->tx_mask_t2_p_cambi_l = $booking['tx_mask_t2_p_cambi_l'];
-//                    $store->tx_mask_t2_p_metodo_b = $booking['tx_mask_t2_p_metodo_b'];
-//                    $store->tx_mask_t3_p_cash_op_cout = $booking['tx_mask_t3_p_cash_op_cout'];
-//                    $store->tx_mask_t3_p_cash_simo = $booking['tx_mask_t3_p_cash_simo'];
-//                    $store->tx_mask_t3_p_check_acconto = $booking['tx_mask_t3_p_check_acconto'];
-//                    $store->tx_mask_t3_p_city_tax_amount = $booking['tx_mask_t3_p_city_tax_amount'];
-//                    $store->tx_mask_t3_p_cleaning_fee_amount = $booking['tx_mask_t3_p_cleaning_fee_amount'];
-//                    $store->tx_mask_t3_p_cw = $booking['tx_mask_t3_p_cw'];
-//                    $store->tx_mask_t3_p_cw_sconto = $booking['tx_mask_t3_p_cw_sconto'];
-//                    $store->tx_mask_t3_p_extra_p = $booking['tx_mask_t3_p_extra_p'];
-//                    $store->tx_mask_t3_p_note_cont = $booking['tx_mask_t3_p_note_cont'];
-//                    $store->tx_mask_t3_p_s_b = $booking['tx_mask_t3_p_s_b'];
-//                    $store->tx_mask_t3_p_s_checkout = $booking['tx_mask_t3_p_s_checkout'];
-//                    $store->tx_mask_t3_p_s_chin = $booking['tx_mask_t3_p_s_chin'];
-//                    $store->tx_mask_t3_p_s_ex_checkout = $booking['tx_mask_t3_p_s_ex_checkout'];
-//                    $store->tx_mask_t3_p_s_extra_checkin = $booking['tx_mask_t3_p_s_extra_checkin'];
-//                    $store->tx_mask_t3_p_saldo_ric_b = $booking['tx_mask_t3_p_saldo_ric_b'];
-//                    $store->tx_mask_t3_p_stay = $booking['tx_mask_t3_p_stay'];
-//                    $store->tx_mask_t4_azioni = $booking['tx_mask_t4_azioni'];
-//                    $store->tx_mask_t4_test_email = $booking['tx_mask_t4_test_email'];
-//                    $store->tx_mask_t5_kross_city_tax_amount = $booking['tx_mask_t5_kross_city_tax_amount'];
-//                    $store->tx_mask_t5_kross_cleaning_fee_amount = $booking['tx_mask_t5_kross_cleaning_fee_amount'];
-//                    $store->tx_mask_t5_kross_cod_channel = $booking['tx_mask_t5_kross_cod_channel'];
-//                    $store->tx_mask_t5_kross_email = $booking['tx_mask_t5_kross_email'];
-//                    $store->tx_mask_t5_kross_id = $booking['tx_mask_t5_kross_id'];
-//                    $store->tx_mask_t5_kross_new = $booking['tx_mask_t5_kross_new'];
-//                    $store->tx_mask_t5_kross_ota_commissions_collected = $booking['tx_mask_t5_kross_ota_commissions_collected'];
-//                    $store->tx_mask_t5_kross_ota_id = $booking['tx_mask_t5_kross_ota_id'];
-//                    $store->tx_mask_t5_kross_other_extra_total_amount = $booking['tx_mask_t5_kross_other_extra_total_amount'];
-//                    $store->tx_mask_t5_kross_payment_total_amount = $booking['tx_mask_t5_kross_payment_total_amount'];
-//                    $store->tx_mask_t6_assistenza_interventol_lastminute = $booking['tx_mask_t6_assistenza_interventol_lastminute'];
-//                    $store->tx_mask_t6_intervento_lastminute = $booking['tx_mask_t6_intervento_lastminute'];
-//                    $store->costo_orario = 0;
-//                    $store->totale_pulizie = 0;
-//                    $store->costo_co = 0;
-//                    $store->mancia_cli = 0;
-//                    $store->costi_costo_operatore_cambio_biancheria = 0;
-//                    $store->costi_costo_kit = 0;
-//
-//                    $store->save();
-//
-//                    if ($store->wasRecentlyCreated === true) {
-//                        $message = 'Created';
-//                    } else {
-//                        $message = 'Updated';
-//                    }
-//
-//                    event(new BookingEvent($store, $message, $booking->uid));
-//
-//                } catch (\Exception $e) {
-//                    event(new BookingErrorEvent('Insert Failed', $booking->uid, $e->getMessage()));
-//                }
-//            });
-//        }
-
-
     }
 }

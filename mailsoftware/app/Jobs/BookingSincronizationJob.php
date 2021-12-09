7aab959c01c124eff00ad8effc33aa73961e10b0
franz177 <giliberti.f@gmail.com>
2021-12-09 13:37:14 +0100

    Removed ->whereNotNull('tx_mask_p_sito') from query

--- mailsoftware/app/Jobs/BookingSincronizationJob.php
+++ mailsoftware/app/Jobs/BookingSincronizationJob.php
@@ -48,12 +48,23 @@ class BookingSincronizationJob implements ShouldQueue
             ->where('deleted', 0)
             ->whereIn(Typo::raw('MONTH(FROM_UNIXTIME(tstamp))'), $months)
             ->where(Typo::raw('YEAR(FROM_UNIXTIME(tstamp))'), '=', $year)
-            ->whereNotNull('tx_mask_p_sito')
+//            ->whereNotNull('tx_mask_p_sito')
             ->whereNotNull('tx_mask_p_casa')
             ->where(Typo::raw('YEAR(tx_mask_p_data_arrivo)'), '>', 2020)
             ->where('header', 'not like', "%test%")
             ->orderBy('uid', 'desc')
             ->get();
+
+//        $bookings = Typo::where('CType','mask_db_alg_pren')
+//            ->where('hidden', 0)
+//            ->where('deleted', 0)
+//            ->whereIn(Typo::raw('uid'), [2319,2315,2318,2316])
+////            ->whereNotNull('tx_mask_p_sito')
+//            ->whereNotNull('tx_mask_p_casa')
+//            ->where(Typo::raw('YEAR(tx_mask_p_data_arrivo)'), '>', 2020)
+//            ->where('header', 'not like', "%test%")
+//            ->orderBy('uid', 'desc')
+//            ->get();
 
         if($bookings){
             $bookings->each(function ($booking) {

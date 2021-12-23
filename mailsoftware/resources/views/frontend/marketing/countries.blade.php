000cbf02361d94e0f9e72ea8f80043882edecec4
franz177 <giliberti.f@gmail.com>
2021-12-23 17:41:49 +0100

    Added countries view in marketing

---
+++ mailsoftware/resources/views/frontend/marketing/countries.blade.php
@@ -0,0 +1,216 @@
+@extends('layouts.template')
+
+@section('styles')
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 col-xxl-12">
+            <div class="card card-custom card-stretch gutter-b">
+                <div class="card-header border-0 pt-5">
+                    <h3 class="card-title align-items-start flex-column">
+                            <span class="card-label font-weight-bolder text-dark text-uppercase">
+                                Prenotazioni per Nazione
+                            </span>
+                        <span class="text-muted mt-3 font-weight-bold font-size-sm"></span>
+                    </h3>
+                </div>
+                <div class="card-body pt-0 pb-3">
+                    <div class="tab-content">
+                        <form action="" method="POST" enctype="multipart/form-data" id="form_flow_text">
+                            @csrf
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <div class="mb-2 d-flex flex-column">
+                                        <div class="input-group has-validation">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="far fa-calendar-alt"></i>
+                                                </span>
+                                            </div>
+                                            <select class="form-control" id="year" name="year">
+                                                <option value="0">Scegli anno</option>
+                                                @foreach($years as $year)
+                                                    <option value="{{ $year->year }}">{{ $year->year }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="mb-2 d-flex flex-column">
+                                        <div class="input-group has-validation">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fas fa-home"></i>
+                                                </span>
+                                            </div>
+                                            <select multiple="multiple" class="form-control" id="house" name="house[]" style="min-height: 150px;">
+                                                @foreach($houses_typo as $uid => $name)
+                                                    <option value="{{ $uid }}">{{ $name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="btn-group" role="group" aria-label="Basic example">
+                                        <button type="button" class="btn btn-light-instagram" name="removeFilter" id="removeFilter">Rimuovi Filtri</button>
+                                        <button type="button" class="btn btn-instagram" name="submit" id="submit">Cerca</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="card-body pt-0 pb-3">
+                    <div class="tab-content">
+                        <!--begin::Table-->
+                        <div class="table-responsive" >
+                            <table class="table table-striped table-bordered  dt-responsive" id="sample_21">
+                                <thead class="text-uppercase">
+                                <tr>
+                                    <th></th>
+                                    <th class="all">Nazione</th>
+                                    <th class="all"># Prenotazioni</th>
+                                    <th class="all">% Su totale</th>
+                                </tr>
+                                </thead>
+                                <tfoot>
+                                <tr>
+                                    <th></th>
+                                    <th class="all text-center"></th>
+                                    <th class="none text-left"></th>
+                                    <th class="all text-uppercase"></th>
+                                </tr>
+                                </tfoot>
+
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer justify-content-between">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+{{-- Scripts Section --}}
+@section('scripts')
+    <script src="{{ asset('js/pages/widgets.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/datatables/datatables.js') }}" type="text/javascript"></script>
+
+    <script type="text/javascript">
+
+        let data_search = new Object();
+
+        $(document).ready(function(){
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                }
+            });
+
+            var tables = $('#sample_21');
+
+            var oTable = tables.DataTable({
+                // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+                "language": {
+                    "aria": {
+                        "sortAscending": ": attivare per ordinare la colonna in ordine crescente",
+                        "sortDescending": ": attivare per ordinare la colonna decrescente"
+                    },
+                    "emptyTable": "Nessun dato trovato nella Tabella",
+                    "info": "Visualizzati da _START_ a _END_ di _TOTAL_ record",
+                    "infoEmpty": "Nessuna voce trovata",
+                    "infoFiltered": "(Filtrato 1 di _MAX_ record totali)",
+                    "lengthMenu": "_MENU_ Righe",
+                    "search": "Cerca:",
+                    "zeroRecords": "Nessuna corrispondenza trovata"
+                },
+
+                processing: true,
+                serverSide: true,
+                fixedHeader: true,
+
+                ajax: {
+                    url:"{{ route('countries.table') }}",
+                    data: function (d) {
+                        d.year = $('select[name=year] option').filter(':selected').val();
+                        d.house = $('#house').val();
+                    }
+                },
+
+                columns: [
+                    {
+                        className:      'hidden',
+                        orderable:      false,
+                        searchable:     false,
+                        data:           null,
+                        defaultContent: '',
+                        cellType:       "th",
+                        sortable:       false
+                    },
+                    {data: 'country.cn_short_it'},                             //td:eq(13)
+                    {data: 'totale'},                          //td:eq(14)
+                    {data: 'percent'},                    //td:eq(15)
+                ],
+
+                rowCallback: function(row, data, index) {
+                },
+
+                footerCallback: function(row, data, index){
+                    var api = this.api(), data;
+                    var tot = data.length - 1;
+                    if(tot > 0){
+                        $( api.column( 2 ).footer() ).html(data[tot].totale_pren);
+                    } else {
+                        $( api.column( 2 ).footer() ).html('');
+                    }
+
+                },
+
+                // setup buttons extentension: http://datatables.net/extensions/buttons/
+                buttons: [
+                    // { extend: 'print', className: 'btn dark btn-outline' },
+                    { extend: 'pdf', className: 'btn green btn-outline' },
+                    { extend: 'excel', className: 'btn purple btn-outline ' }
+                ],
+
+                // setup responsive extension: http://datatables.net/extensions/responsive/
+                // setup responsive extension: http://datatables.net/extensions/responsive/
+                responsive: {
+                    details: {
+                        type: 'column',
+                        target: 'tr',
+                    }
+                },
+                columnDefs: [
+                    { className: 'control', targets:   0, width: '3%' }, //plus
+                    { width: '10%', targets: 2}, //hous    ],
+                    { width: '10%', targets: 3}, //hous    ],
+                ],
+
+                "lengthMenu": [
+                    [5, 10, 15, 20, -1],
+                    [5, 10, 15, 20, "All"] // change per page values here
+                ],
+
+                // set the initial value
+                "pageLength": -1,
+
+                "dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'B f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>", // horizobtal scrollable datatable
+            });
+
+            $('#submit').on('click', function(e) {
+                oTable.draw();
+                e.preventDefault();
+            });
+            $('#removeFilter').on('click', function(e) {
+                location.reload();
+            });
+        });
+
+    </script>
+@endsection

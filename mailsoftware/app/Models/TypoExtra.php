efb7558549dec3db10d4f5a9a2401fa8b7953103
franz177 <giliberti.f@gmail.com>
2021-12-28 18:34:44 +0100

    Added model for tx_mask_t5_repeat_metodo table on Typo3 Database

---
+++ mailsoftware/app/Models/TypoExtra.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TypoExtra extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tx_mask_t5_repeat_metodo';
+}
